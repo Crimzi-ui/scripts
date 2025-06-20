358f2cf66d5817f33e1ee5a5f046ac2a0a48719e
Crimzi-ui <raidynhammer@icloud.com>
2025-06-20 15:48:07 -0600

    Initial

--- main.lua
+++ main.lua
@@ -1,3 +1,6 @@
 if game.PlaceId == 6884319169 then
   print("Mic Up")
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Crimzi-ui/scripts/refs/heads/main/micup.lua"))()
+else
+  print("no game found for this game we do not support it")
 end
